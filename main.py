a80491cf4cc13e9fa9bd1459bf4b06d509223e67
qeruwo <qeruwork@mail.ru>
2025-05-19 19:26:23 +0300

    1st commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+import signal
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters.command import Command
+from aiogram.types import Message
+
+#из файлов // from files
+
+from help.config_token import token
+bot = Bot(token=token)
+dp = Dispatcher()
+
+# Включаем логирование // start logging
+logging.basicConfig(level=logging.INFO)
+
+# Хэндлер на команду /start // handler for command /start
+@dp.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer("hi")
+
+    
+# Запуск процесса поллинга новых апдейтов // Starting the polling process for new updates
+async def main():
+    dp.start_polling(bot)
+
+#Запуск // start main.py
+
+if __name__ == "__main__":
+    asyncio.run(main())
